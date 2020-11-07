121a9a9c3a0871c0977f927e4107cd9c1df6a473
Adnane Aabbar <44345798+adnaneaabbar@users.noreply.github.com>
2020-11-07 18:12:25 +0100

    dp init

---
+++ dicecombinations.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    int mod = 1e9 + 7;
+    cin >> n;
+    int dp[n + 1] = {0};
+    dp[0] = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= 6; j++)
+        {
+            if (i >= j)
+            {
+                dp[i] = (dp[i] + dp[i - j]) % mod;
+            }
+        }
+    }
+
+    cout << dp[n] << endl;
+}
